b7ee9f307c20d224af4e4a04b3f3915daa20ea86
mnygp <markus.markusnp@gmail.com>
2025-08-13 10:10:03 +0200

    Fixed band gap convergence script

--- scripts/convergence/band-gap-convergence.py
+++ scripts/convergence/band-gap-convergence.py
@@ -1,23 +1,22 @@
 from functions.structure import create_bilayer
 from functions.bandstructure import calc_gap
+from ase.parallel import parprint
 
+kpts = list(range(10, 37, 2))
 
 for z in [6.3, 6.5, 6.8]:
-    print(f'Z distance: {z} Å')
+    parprint(f'Z distance: {z} Å')
     for shift in [0, 0.1, 0.2]:
-        print("Shift:", shift)
-
-        kpts = list(range(10, 37, 2))
-        gap_arr = []
+        parprint("Shift:", shift)
 
         for k in kpts:
             bilayer = create_bilayer(z_dist=6.6, constrain=True,
                                      a_shift=shift, b_shift=shift)
 
             gap = calc_gap(bilayer, kpts=k)
+            parprint(f'k-points: {k}, Band gap: {gap:.5f} eV')
 
-            gap_arr.append(gap)
-            print(f'k-points: {k}, Band gap: {gap:.3f} eV')
-        print(" ")
-    print("--------------------------------------------------")
-    print(" ")
+        parprint(" ")
+
+    parprint("--------------------------------------------------")
+    parprint(" ")
