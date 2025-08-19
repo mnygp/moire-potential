d1a00bdf06e6dac020b80b72c76f456426b21b0c
mnygp <markus.markusnp@gmail.com>
2025-08-19 13:19:05 +0200

    Moved a folder

--- scripts/hybridization/diagonal/plotting.py
+++ scripts/hybridization/diagonal/plotting.py
@@ -6,6 +6,8 @@ import scienceplots  # noqa: F401
 plt.style.use('science')
 fig, ax = plt.subplots(2, 1, figsize=(6, 4.5), sharex=True,
                        gridspec_kw={'height_ratios': [1, 2]})
+fig2, ax2 = plt.subplots(2, 1, figsize=(6, 4.5), sharex=True,
+                         gridspec_kw={'height_ratios': [1, 2]})
 
 for a, color, label in zip([3.184, 3.2515, 3.319],
                            ['C0', 'C1', 'C2'],
@@ -21,6 +23,11 @@ for a, color, label in zip([3.184, 3.2515, 3.319],
     ax[1].plot(shift_arr, z_dist_arr, '-o',
                label=label, color=color)
 
+    ax2[0].plot(shift_arr, gap_arr, '-o',
+                label=label, color=color)
+    ax2[1].plot(shift_arr, z_dist_arr, '-o',
+                label=label, color=color)
+
 
 ax[0].set_ylabel('Band Gap (eV) \n (Relative to min)')
 ax[0].grid()
@@ -28,5 +35,14 @@ ax[1].set_xlabel('Shift')
 ax[1].set_ylabel('Z distance (Å)')
 ax[1].legend()
 ax[1].grid()
-plt.tight_layout()
-plt.savefig('gap_shift_plot.png', dpi=500)
+fig.tight_layout()
+fig.savefig('gap_shift_plot_norm.png', dpi=500)
+
+ax2[0].set_ylabel('Band Gap (eV)')
+ax2[0].grid()
+ax2[1].set_xlabel('Shift')
+ax2[1].set_ylabel('Z distance (Å)')
+ax2[1].legend()
+ax2[1].grid()
+fig2.tight_layout()
+fig2.savefig('gap_shift_plot.png', dpi=500)
