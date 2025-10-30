be3acf81df1891b26e4ac110ab007b8bba06d50d
mnygp <markus.markusnp@gmail.com>
2025-10-30 10:46:20 +0100

    Comparing DFT to MACE and MatterSim in the small cell

---
+++ scripts/geometry/ML_comparison/plotting.py
@@ -0,0 +1,142 @@
+from ase.io import read
+import numpy as np
+import matplotlib.pyplot as plt
+from functions.geometry import interlayer_distance, strain
+
+
+def scatter_plot_color(X: np.ndarray, Y: np.ndarray, Z: np.ndarray,
+                       filename: str, title: str, color_map: str,
+                       label: str, strings=None):
+
+    fig, ax = plt.subplots()
+
+    scatter = ax.scatter(X, Y, c=Z, cmap=color_map, s=50, edgecolor='none')
+    fig.colorbar(scatter, ax=ax, label=label)
+
+    ax.set_xlabel("X Position [Å]")
+    ax.set_ylabel("Y Position [Å]")
+    ax.axis('equal')
+    ax.set_title(title)
+
+    if strings is not None:
+        text = '\n'.join(strings)
+        ax.text(0.03, 0.17, text, transform=ax.transAxes, fontsize=9,
+                verticalalignment='top')
+
+    fig.savefig(f"plots/{filename}.png", dpi=300, bbox_inches='tight')
+    plt.close(fig)
+
+mace_atoms = read('structure_MACE.json')
+mattersim_atoms = read('structure_mattersim.json')
+dft_atoms = read('structure_DFT.json')
+
+structs = [dft_atoms, mace_atoms, mattersim_atoms]
+
+
+x, y, z_dft = interlayer_distance(dft_atoms)
+_, _, Mo_strain_dft = strain(dft_atoms, 'Mo')
+_, _, W_strain_dft = strain(dft_atoms, 'W')
+
+_, _, z_mace = interlayer_distance(mace_atoms)
+_, _, Mo_strain_mace = strain(mace_atoms, 'Mo')
+_, _, W_strain_mace = strain(mace_atoms, 'W')
+
+_, _, z_mattersim = interlayer_distance(mattersim_atoms)
+_, _, Mo_strain_mattersim = strain(mattersim_atoms, 'Mo')
+_, _, W_strain_mattersim = strain(mattersim_atoms, 'W')
+
+for s, name in zip(structs, ['DFT', 'MACE', 'MatterSim']):
+    z_dist_x, z_dist_y, z_dist = interlayer_distance(s)
+    Mo_strain_x, Mo_strain_y, Mo_strain = strain(s, 'Mo')
+    W_strain_x, W_strain_y, W_strain = strain(s, 'W')
+
+    if (name != 'DFT'):
+        z_error = np.mean(z_dft - z_dist)
+        z_error_std = np.std(z_dft - z_dist)
+        print(f'Average error in z dist for {name}: {z_error:.3f} +/- {z_error_std:.3f}')
+        print(f'Error range for {name}: {min(z_dft - z_dist) - max(z_dft - z_dist):.4f}')
+        print()
+
+        Mo_error = np.mean(Mo_strain_dft - Mo_strain)
+        Mo_error_std = np.std(Mo_strain_dft - Mo_strain)
+        print(f'Average error in Mo strain for {name}: {Mo_error:.5f} +/- {Mo_error_std:.5f}')
+
+        W_error = np.mean(W_strain_dft - W_strain)
+        W_error_std = np.std(W_strain_dft - W_strain)
+        print(f'Average error in W strain for {name}: {W_error:.5f} +/- {W_error_std:.5f}')
+
+mace_dif = z_mace - z_dft
+mattersim_dif = z_mattersim - z_dft
+print("-----------------------")
+print("DFT z interval:")
+print(max(z_dft), min(z_dft))
+print(max(z_dft) - min(z_dft))
+
+print("Mace z interval:")
+print(max(z_mace), min(z_mace))
+print(max(z_mace) - min(z_mace))
+
+
+print("MatterSim z interval:")
+print(max(z_mattersim), min(z_mattersim))
+print(max(z_mattersim) - min(z_mattersim))
+print("----------------------")
+
+max_val = max(max(mace_dif), max(mattersim_dif))
+min_val = min(min(mace_dif), min(mattersim_dif))
+
+fig, ax = plt.subplots(figsize=(7, 5))
+
+ax.hist(mace_dif, bins=100, range=(min_val, max_val),
+        alpha=0.8, label='MACE')
+ax.hist(mattersim_dif, bins=100, range=(min_val, max_val),
+        alpha=0.8, label='MatterSim')
+
+ax.vlines(0, 0, 25, colors='black', linestyles='dashed', label='DFT reference')
+
+ax.set_xlabel('Z distance difference [Å]')
+ax.set_ylabel('Count')
+ax.legend()
+ax.set_title('The difference in interlayer distance compared to DFT')
+ax.grid(True)
+fig.tight_layout()
+
+fig.savefig('plots/z_distance_difference_histogram.png', dpi=500)
+plt.close(fig)  # optional: closes figure if running in loops
+
+
+DFT_data = np.genfromtxt('DFT_data.csv', delimiter=',', skip_header=1)
+MACE_data = np.genfromtxt('MACE_data.csv', delimiter=',', skip_header=1)
+MAtterSim_data = np.genfromtxt('MatterSim_data.csv', delimiter=',', skip_header=1)
+
+shift = DFT_data[:, 0]
+# z distance [:, 1]
+# vec1 [:, 2]
+# vec2 [:, 3]
+# gap [:, 4]
+
+fig1, ax1 = plt.subplots(nrows=3, ncols=1, sharex=True, figsize=(7, 9))
+
+
+for data, color, name in zip([DFT_data, MACE_data, MAtterSim_data],
+                             ['C0', 'C1', 'C2'],
+                             ['DFT', 'MACE', 'MatterSim']):
+    z_dist = data[:, 1]
+    average_vec = (data[:, 2] + data[:, 3]) / 2
+    gap = data[:, 4]
+
+    ax1[0].plot(shift, z_dist, '-o', color=color)
+    ax1[1].plot(shift, (average_vec - 3.184)/3.184 * 100, '-o', color=color)
+    ax1[2].plot(shift, gap, '-o', color=color, label=name)
+
+ax1[2].set_xlabel('Shift [fraction of lattice vector]')
+ax1[0].set_ylabel('Interlayer distance [Å]')
+ax1[1].set_ylabel('Lattice deviation from MoS2 [%]')
+ax1[2].set_ylabel('Band gap [eV]')
+ax1[2].legend()
+ax1[0].set_title('Comparison of DFT, MACE and MatterSim')
+ax1[0].grid(True)
+ax1[1].grid(True)
+ax1[2].grid(True)
+fig1.tight_layout()
+fig1.savefig('plots/DFT_MACE_MatterSim_comparison.png', dpi=500)
---
+++ scripts/hybridization/diagonal-shift/ML-comparison/relax.py
@@ -0,0 +1,129 @@
+from ase import Atoms
+from ase.build import mx2
+from ase.constraints import FixedLine
+from ase.optimize import BFGS
+from ase.filters import FrechetCellFilter
+from ase.parallel import parprint
+from ase.calculators.dftd3 import DFTD3
+from gpaw import GPAW, PW, FermiDirac
+import numpy as np
+import csv
+
+def create_bilayer(z_dist: float, lattice_length: float = 3.2515,
+                   a_shift: float = 0, b_shift: float = 0,
+                   constrain: bool = False,
+                   acute_corner: bool = True) -> Atoms:
+
+    MoS2 = mx2('MoS2', a=lattice_length, vacuum=6.0)
+    WSe2 = mx2('WSe2', a=lattice_length, vacuum=6.0)
+
+    # 6.6Å of distance between layers
+    MoS2.positions[:, 2] += z_dist/2
+    WSe2.positions[:, 2] -= z_dist/2
+
+    # Create the initial structure
+    struct = WSe2 + MoS2
+    struct.center(vacuum=10.0, axis=2)
+
+    if constrain:
+        indices = [atom.index for atom in struct if (atom.symbol == 'W' or
+                                                     atom.symbol == 'Mo')]
+        struct.set_constraint(FixedLine(indices=indices, direction=[0, 0, 1]))
+
+    if acute_corner:
+        struct.positions += struct.cell[0]
+        for atom in struct:
+            if atom.symbol == 'Mo' or atom.symbol == 'S':
+                atom.position -= a_shift * struct.cell[0]
+                atom.position += b_shift * struct.cell[1]
+    else:
+        for atom in struct:
+            if atom.symbol == 'Mo' or atom.symbol == 'S':
+                atom.position += a_shift*struct.cell[0]
+                atom.position += b_shift*struct.cell[1]
+
+    struct.pbc = True
+    struct.wrap()
+
+    return struct
+
+
+def calc_gap(atoms, functional: str = "PBE",
+             kpts: int = 18, pw_cut: float = 500) -> tuple[float, float, float]:
+
+    calc = GPAW(mode=PW(pw_cut),  # Basis set
+                xc=functional,  # Functional
+                kpts={'size': (kpts, kpts, 1)},  # k-points
+                occupations=FermiDirac(0.01),
+                txt=None)
+
+    atoms.calc = calc
+    atoms.get_potential_energy()
+
+    homo, lumo = calc.get_homo_lumo()
+
+    return lumo - homo
+
+a = 3.2515  # Average lattice length
+
+shift_arr = np.linspace(0, 1, 30)
+z_arr = []
+vec1_arr = []
+vec2_arr = []
+gap_arr = []
+
+for shift in shift_arr:
+    struct = create_bilayer(6.6,
+                            lattice_length=a,
+                            a_shift=shift,
+                            b_shift=shift,
+                            constrain=True,
+                            acute_corner=True)
+
+
+    struct.pbc = [1, 1, 1]
+
+    calc = GPAW(mode=PW(500),  # Basis set
+                xc='PBE',  # Functional
+                kpts={'size': (8, 8, 1)},  # k-points
+                occupations=FermiDirac(0.01),
+                txt=None)
+    struct.calc = DFTD3(dft=calc)
+
+    opt_filter = FrechetCellFilter(struct, mask=[1, 1, 0, 0, 0, 1])
+    opt = BFGS(opt_filter, logfile=f'logs/relax_{shift:.2f}.log', trajectory=f'traj_files/relax_{shift:.2f}.traj')
+    opt.run(fmax=0.01)
+
+    parprint('Starting gap calc')
+    gap = calc_gap(struct, kpts=30)
+    parprint('The gap is:')
+    parprint(gap)
+    
+    symbols = np.array(struct.get_chemical_symbols())
+    relaxed_z_dist = abs(struct[symbols == 'W'].positions[0][2]
+                          - struct[symbols == 'Mo'].positions[0][2])
+    z_arr.append(relaxed_z_dist)
+    vec1_arr.append(np.linalg.norm(struct.cell[0, :2]))
+    vec2_arr.append(np.linalg.norm(struct.cell[1, :2]))
+    gap_arr.append(gap)
+
+    parprint(f'Shift: {shift}')
+    parprint(relaxed_z_dist)
+    parprint(struct.cell[0, :2])
+    parprint(struct.cell[1, :2])
+    parprint(gap)
+
+
+# Write to CSV
+with open("DFT_data.csv", mode="w", newline="") as f:
+    writer = csv.writer(f)
+    
+    # Optional: write header
+    writer.writerow(["shift", "z", "vec1", "vec2", "gap"])
+    
+    # Write data rows
+    for s, z, v1, v2, g in zip(shift_arr, z_arr, vec1_arr, vec2_arr, gap_arr):
+        writer.writerow([s, z, v1, v2, g])
+
+
+
